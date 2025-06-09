3d1299e5b8503f3396420f090a27b58ee6edd679
YeonsuJ <jysjyg34@gmail.com>
2025-06-09 18:58:05 +0900

    버튼 눌림 시간 기반 DC 모터 PWM 제어 및 RPM 계산 후 OLED 실시간 상태 출력 로직 추가

---
+++ Unit_car_central/Core/Src/gpio.c
@@ -0,0 +1,106 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+     PA2   ------> USART2_TX
+     PA3   ------> USART2_RX
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB0 PB1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB4 PB5 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI0_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI1_IRQn);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
--- Unit_car_central/Core/Src/main.c
+++ Unit_car_central/Core/Src/main.c
@@ -18,10 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "i2c.h"
+#include "tim.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "fonts.h"
+#include "ssd1306.h"
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,27 +47,45 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-TIM_HandleTypeDef htim3;
-TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
 int16_t last_encoder = 0;
 float motor_rpm = 0;
 int motor_direction = 0; // 0: forward, 1: backward
+
+uint8_t accel_pressed = 0, brake_pressed = 0;
+uint32_t accel_count = 0, brake_count = 0;
+uint32_t prev_tick_accel = 0, prev_tick_brake = 0;
+uint32_t last_tick = 0;
+
+int duty = 0;
+int accel_step = 5;
+int max_duty = 1000;
+int min_duty = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_TIM3_Init(void);
-static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
-
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+//void Update_RPMA_Direction(void)
+//{
+//    int16_t enc = __HAL_TIM_GET_COUNTER(&htim3);
+//    int32_t delta = (int16_t)(enc - last_encoder);  // 오버플로우 보정
+//    last_encoder = enc;
+//
+//    motor_rpm = ((float)delta / TICKS_PER_REV) * 10.0f * 60.0f;
+//
+//    if (__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim3))
+//        motor_direction = 1;
+//    else
+//        motor_direction = 0;
+//
+//    last_tick = HAL_GetTick();
+//}
 /* USER CODE END 0 */
 
 /**
@@ -96,76 +119,78 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM3_Init();
   MX_TIM4_Init();
+  MX_I2C2_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  SSD1306_Init();
+
   HAL_TIM_Encoder_Start(&htim3, TIM_CHANNEL_ALL);
   HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+  HAL_TIM_Base_Start_IT(&htim2);
 
-  uint32_t last_tick = HAL_GetTick();
-  uint32_t motor_control_tick = 0;
-  uint8_t state = 0;
+//  uint32_t last_tick = HAL_GetTick();
+//  uint32_t motor_control_tick = 0;
+//  uint8_t state = 0;
+//
+//  static uint32_t state_durations[] = {5000, 2000, 5000, 2000};  // 각 상태 지속시간 (ms)
 
-  static uint32_t state_durations[] = {5000, 2000, 5000, 2000};  // 각 상태 지속시간 (ms)
-
+  // 모터 전진 방향 설정
+   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);  // in1 = 0
+   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);    // in2 = 1
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  if (HAL_GetTick() - last_tick >= 20)
-	  	    {
-	  		  int16_t enc = __HAL_TIM_GET_COUNTER(&htim3);
-	  		  int32_t delta = (int16_t)(enc - last_encoder);  // 강제 형변환을 통해 오버플로우 보정
-	  	      last_encoder = enc;
+//	  if (HAL_GetTick() - last_tick >= 20)
+//	      {
+//	          Update_RPMA_Direction();
+//	      }
 
-	  	      motor_rpm = ((float)delta / TICKS_PER_REV) * 10.0f * 60.0f;
-
-	  	      if (__HAL_TIM_IS_TIM_COUNTING_DOWN(&htim3))
-	  	        motor_direction = 1;
-	  	      else
-	  	        motor_direction = 0;
-
-	  	      last_tick = HAL_GetTick();
-	  	    }
-
-	  	  // 상태 기반으로 항상 핀/모터 상태 적용
-	  	  switch (state)
-	  	  {
-	  	    case 0: // forward
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
-	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 300);
-	  	      break;
-	  	    case 1: // stop
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 0);
-	  	      break;
-	  	    case 2: // backward
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 600);
-	  	      break;
-	  	    case 3: // stop again
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
-	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
-	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 0);
-	  	      break;
-	  	  }
-
-	  	  // 상태 전이 조건은 별도로 유지
-	  	  if (HAL_GetTick() - motor_control_tick >= state_durations[state])
-	  	  {
-	  	      state = (state + 1) % 4;
-	  	      motor_control_tick = HAL_GetTick();
-	  	  }
+//	  if (HAL_GetTick() - last_tick >= 20)
+//	  	    {
+//		  	  Update_RPMA_Direction();
+//	  	    }
+//
+//	  	  // 상태 기반으로 항상 핀/모터 상태 적용
+//	  	  switch (state)
+//	  	  {
+//	  	    case 0: // forward
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_SET);
+//	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 300);
+//	  	      break;
+//	  	    case 1: // stop
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+//	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 0);
+//	  	      break;
+//	  	    case 2: // backward
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+//	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 600);
+//	  	      break;
+//	  	    case 3: // stop again
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+//	  	      HAL_GPIO_WritePin(GPIOB, GPIO_PIN_5, GPIO_PIN_RESET);
+//	  	      __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, 0);
+//	  	      break;
+//	  	  }
+//
+//	  	  // 상태 전이 조건은 별도로 유지
+//	  	  if (HAL_GetTick() - motor_control_tick >= state_durations[state])
+//	  	  {
+//	  	      state = (state + 1) % 4;
+//	  	      motor_control_tick = HAL_GetTick();
+//}
 
     /* USER CODE END WHILE */
-
+ }
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
 }
+  /* USER CODE END 3 */
+
 
 /**
   * @brief System Clock Configuration
@@ -221,173 +246,117 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM3_Init(void)
-{
-
-  /* USER CODE BEGIN TIM3_Init 0 */
-
-  /* USER CODE END TIM3_Init 0 */
-
-  TIM_Encoder_InitTypeDef sConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM3_Init 1 */
-
-  /* USER CODE END TIM3_Init 1 */
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 0;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 65535;
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
-  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
-  sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
-  sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC1Filter = 10;
-  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
-  sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
-  sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
-  sConfig.IC2Filter = 10;
-  if (HAL_TIM_Encoder_Init(&htim3, &sConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM3_Init 2 */
-
-  /* USER CODE END TIM3_Init 2 */
-
-}
-
-/**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM4_Init(void)
-{
-
-  /* USER CODE BEGIN TIM4_Init 0 */
-
-  /* USER CODE END TIM4_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
-
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
-  htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 90-1;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 1000-1;
-  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  if (HAL_TIM_PWM_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
-  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
-  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  if (HAL_TIM_PWM_ConfigChannel(&htim4, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM4_Init 2 */
-
-  /* USER CODE END TIM4_Init 2 */
-  HAL_TIM_MspPostInit(&htim4);
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : B1_Pin */
-  GPIO_InitStruct.Pin = B1_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA2 PA3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PB4 PB5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_4|GPIO_PIN_5;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
 /* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    uint32_t now = HAL_GetTick();
 
+    if (GPIO_Pin == GPIO_PIN_0)  // Accel 버튼
+    {
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0) == GPIO_PIN_RESET)
+        {
+            // 눌림이 감지된 순간(하강), 디바운싱 여부와 상관없이 초기화
+            accel_pressed = 1;
+            accel_count = 0;
+            prev_tick_accel = now;
+        }
+        else
+        {
+            // 떼짐(상승)은 즉시 pressed=0 처리
+            accel_pressed = 0;
+            prev_tick_accel = now;
+        }
+    }
+    else if (GPIO_Pin == GPIO_PIN_1)  // Brake 버튼
+    {
+        if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_1) == GPIO_PIN_RESET)
+        {
+            // 눌림이 감지된 순간(하강), 디바운싱 여부와 상관없이 초기화
+            brake_pressed = 1;
+            brake_count = 0;
+            prev_tick_brake = now;
+        }
+        else
+        {
+            // 떼짐(상승)은 즉시 pressed=0 처리
+            brake_pressed = 0;
+            prev_tick_brake = now;
+        }
+    }
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+
+	if (htim->Instance == TIM2)
+	    {
+			// 1) RPM 업데이트
+			int16_t enc = __HAL_TIM_GET_COUNTER(&htim3);
+			int32_t delta = (int16_t)(enc - last_encoder);
+			last_encoder = enc;
+
+			motor_rpm = ((float)delta / TICKS_PER_REV) * 50.0f * 60.0f;
+			// 주기 20ms이므로 1초에 50번 호출됨 → ×50 ×60 으로 환산
+
+	        // 2) 버튼 누름 시간 카운트
+	        if (accel_pressed) accel_count++;
+	        if (brake_pressed) brake_count++;
+
+
+	        // 3) PWM duty 제어
+	        if (brake_pressed) //brake부터 체크해야 동시에 누를 때 brake가 우선처리됨
+	        {
+	        	duty -= accel_step * 5;
+	        }
+	        else if (accel_pressed)
+	        {
+	        	if (duty ==0) duty = 200; // deadzone으로 인해 초기값 수정
+	        	else duty += accel_step;
+	        }
+	        else
+	        {
+	        	duty -= accel_step;
+	        }
+
+	        // 4) duty 값 클램핑 (음수 또는 max 이상 방지)
+			if (duty < 0) duty = 0;
+			if (duty > max_duty) duty = max_duty;
+
+	        __HAL_TIM_SET_COMPARE(&htim4, TIM_CHANNEL_3, duty);
+
+
+			// 4) OLED 출력
+			char buffer1[20], buffer2[20];
+			SSD1306_Clear(); // 화면 초기화 설정
+			snprintf(buffer1, sizeof(buffer1), "DUTY: %.1f", (float)duty);
+			SSD1306_GotoXY(0, 0);
+			SSD1306_Puts(buffer1, &Font_11x18, 1);
+			snprintf(buffer2, sizeof(buffer2), "RPM: %.1f", motor_rpm);
+			SSD1306_GotoXY(0, 20);
+	        SSD1306_Puts(buffer2, &Font_11x18, 1);
+			SSD1306_UpdateScreen();
+
+	        }
+
+
+//    if (htim->Instance == TIM2) // 20ms 주기 타이머
+//    {
+//        if (accel_pressed) accel_count++;
+//        if (brake_pressed) brake_count++;
+//
+//        char buffer1[20], buffer2[20];
+//
+//        snprintf(buffer1, sizeof(buffer1), "ACC: %lums", accel_count * 20);
+//        SSD1306_GotoXY(0, 0);
+//        SSD1306_Puts(buffer1, &Font_11x18, 1);
+//
+//        snprintf(buffer2, sizeof(buffer2), "BRK: %lums", brake_count * 20);
+//        SSD1306_GotoXY(0, 20);
+//        SSD1306_Puts(buffer2, &Font_11x18, 1);
+//
+//        SSD1306_UpdateScreen();
+//    }
+}
 /* USER CODE END 4 */
 
 /**
