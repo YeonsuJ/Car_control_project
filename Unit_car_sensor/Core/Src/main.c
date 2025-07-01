e1316b007e2b4ca462092e7ce0a50db136938af6
YeonsuJ <jysjyg34@gmail.com>
2025-07-01 20:10:50 +0900

    (F103C8T6) CAN 통신 송신부(TX) 초기 기능 구현

--- Unit_car_sensor/Core/Src/main.c
+++ Unit_car_sensor/Core/Src/main.c
@@ -18,6 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "can.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -40,29 +42,31 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
-#define TRIG_PIN GPIO_PIN_9
-#define TRIG_PORT GPIOA
-volatile uint32_t ic_val1 = 0;
-volatile uint32_t ic_val2 = 0;
-volatile uint8_t is_first_captured = 0;
-volatile uint32_t distance = 0;
+
+// 초음파센서
+//#define TRIG_PIN GPIO_PIN_9
+//#define TRIG_PORT GPIOA
+//volatile uint32_t ic_val1 = 0;
+//volatile uint32_t ic_val2 = 0;
+//volatile uint8_t is_first_captured = 0;
+//volatile uint32_t distance = 0;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
-void delay_us(uint16_t us);
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
+//void delay_us(uint16_t us);
+//void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+CAN_TxHeaderTypeDef TxHeader;
+uint32_t TxMailbox;
+uint8_t TxData[8]="YEONSU-J";
 /* USER CODE END 0 */
 
 /**
@@ -94,11 +98,18 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_TIM4_Init();
+  MX_CAN_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_1);
+  HAL_CAN_Start(&hcan);
+  TxHeader.DLC = 8;  // data length
+  TxHeader.IDE = CAN_ID_STD;
+  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.StdId = 0x6A5;  // ID can be between Hex1 and Hex7FF (1-2047 decimal)
 
-  uint32_t prev_tick = 0;
+// 초음파센서
+//  HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_1);
+//
+//  uint32_t prev_tick = 0;
 
   /* USER CODE END 2 */
 
@@ -106,28 +117,31 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  // 20ms 주기 체크
-	  if (HAL_GetTick() - prev_tick >= 100)
-	  {
-		  prev_tick = HAL_GetTick();
-
-		  // 1) 초음파 트리거
-		  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);
-		  delay_us(10);
-		  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);
-
-		  // 2) 진동 모터 제어
-		  if (distance <= 10 && distance > 0)
-		  {
-			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET); // 모터1 ON
-			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET); // 모터2 ON
-		  }
-		  else
-		  {
-			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET); // 모터1 OFF
-			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET); // 모터2 OFF
-		  }
-	  }
+	  HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox);
+	  HAL_Delay(1000);
+// 초음파센서
+//	  // 20ms 주기 체크
+//	  if (HAL_GetTick() - prev_tick >= 100)
+//	  {
+//		  prev_tick = HAL_GetTick();
+//
+//		  // 1) 초음파 트리거
+//		  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);
+//		  delay_us(10);
+//		  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);
+//
+//		  // 2) 진동 모터 제어
+//		  if (distance <= 10 && distance > 0)
+//		  {
+//			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET); // 모터1 ON
+//			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET); // 모터2 ON
+//		  }
+//		  else
+//		  {
+//			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET); // 모터1 OFF
+//			  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_RESET); // 모터2 OFF
+//		  }
+//	  }
 
     /* USER CODE END WHILE */
 
@@ -175,127 +189,39 @@ void SystemClock_Config(void)
   }
 }
 
-/**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM4_Init(void)
-{
-
-  /* USER CODE BEGIN TIM4_Init 0 */
-
-  /* USER CODE END TIM4_Init 0 */
-
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_IC_InitTypeDef sConfigIC = {0};
-
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
-  htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 71;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 65535;
-  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_IC_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
-  sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
-  sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-  sConfigIC.ICFilter = 0;
-  if (HAL_TIM_IC_ConfigChannel(&htim4, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM4_Init 2 */
-
-  /* USER CODE END TIM4_Init 2 */
-
-}
-
-/**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-
-  /*Configure GPIO pins : PB10 PB11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
-}
-
 /* USER CODE BEGIN 4 */
-void delay_us(uint16_t us)
-{
-  __HAL_TIM_SET_COUNTER(&htim4, 0);
-  while (__HAL_TIM_GET_COUNTER(&htim4) < us);
-}
 
-void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
-{
-  if (htim->Instance == TIM4 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
-  {
-    if (is_first_captured == 0)
-    {
-      ic_val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
-      is_first_captured = 1;
-    }
-    else
-    {
-      ic_val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
-      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
-      __HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
-
-      uint32_t diff = (ic_val2 > ic_val1) ? (ic_val2 - ic_val1) : (0xFFFF - ic_val1 + ic_val2);
-      distance = (diff * 0.0343f) / 2.0f;
-      is_first_captured = 0;
-
-      __HAL_TIM_ENABLE_IT(htim, TIM_IT_CC1);
-    }
-  }
-}
+//초음파센서
+//void delay_us(uint16_t us)
+//{
+//  __HAL_TIM_SET_COUNTER(&htim4, 0);
+//  while (__HAL_TIM_GET_COUNTER(&htim4) < us);
+//}
+//
+//void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+//{
+//  if (htim->Instance == TIM4 && htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+//  {
+//    if (is_first_captured == 0)
+//    {
+//      ic_val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+//      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+//      is_first_captured = 1;
+//    }
+//    else
+//    {
+//      ic_val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
+//      __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+//      __HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
+//
+//      uint32_t diff = (ic_val2 > ic_val1) ? (ic_val2 - ic_val1) : (0xFFFF - ic_val1 + ic_val2);
+//      distance = (diff * 0.0343f) / 2.0f;
+//      is_first_captured = 0;
+//
+//      __HAL_TIM_ENABLE_IT(htim, TIM_IT_CC1);
+//    }
+//  }
+//}
 /* USER CODE END 4 */
 
 /**
