89800bed5bbd43df382bfc1d63d63a506a9933f4
YeonsuJ <jysjyg34@gmail.com>
2025-07-01 20:29:23 +0900

    (F103C8T6) CAN 통신 수신부(RX) 초기 기능 구현

--- Unit_car_status/Core/Src/main.c
+++ Unit_car_status/Core/Src/main.c
@@ -18,15 +18,16 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "adc.h"
-#include "i2c.h"
+#include "can.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdio.h>
-#include "fonts.h"
-#include "ssd1306.h"
+
+//adc_battery_oled
+//#include <stdio.h>
+//#include "fonts.h"
+//#include "ssd1306.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -58,38 +59,47 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-// --- adc 전압출력 및 배터리 전압 → 퍼센트 계산 함수 ---
-float Read_Battery_Percentage(float* vout_ret)
+//can_rx
+CAN_FilterTypeDef sFilterConfig;
+CAN_RxHeaderTypeDef RxHeader;
+uint8_t RxData[8];
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
 {
-	float correction = 3.08/3.04f; // 수치 보정을 위한 보정계수(멀티미터 측정 : 3.15V -> ADC 3.11V 출력)
-
-	HAL_ADC_Start(&hadc1);
-	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
-	uint16_t adc_val = HAL_ADC_GetValue(&hadc1);
-	HAL_ADC_Stop(&hadc1);
-
-	// 1. ADC → STM32 입력 전압 (Vout)
-	float vout_raw = (3.3f * adc_val) / 4095.0f;
-
-	// 2. 보정 계수 적용
-	float vout = vout_raw * correction;
-;
-
-	// 3. Vout → Vbat (실측 저항값 반영)
-	float vbat = vout * ((19.7f + 6.73f) / 6.73f);  // ≈ 3.9272
-
-	// 4. Vbat → 퍼센트 계산
-	float percent = ((vbat - 9.6f) / (12.6f - 9.6f)) * 100.0f;
-
-	if (percent > 100.0f) percent = 100.0f;
-	if (percent < 0.0f) percent = 0.0f;
-
-	// 5. vout 값 반환 (OLED 출력용)
-	if (vout_ret != NULL)
-		*vout_ret = vout;
-
-	return percent;
+  HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, RxData);
 }
+
+// --- adc 전압출력 및 배터리 전압 → 퍼센트 계산 함수 ---
+//float Read_Battery_Percentage(float* vout_ret)
+//{
+//	float correction = 3.08/3.04f; // 수치 보정을 위한 보정계수(멀티미터 측정 : 3.15V -> ADC 3.11V 출력)
+//
+//	HAL_ADC_Start(&hadc1);
+//	HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+//	uint16_t adc_val = HAL_ADC_GetValue(&hadc1);
+//	HAL_ADC_Stop(&hadc1);
+//
+//	// 1. ADC → STM32 입력 전압 (Vout)
+//	float vout_raw = (3.3f * adc_val) / 4095.0f;
+//
+//	// 2. 보정 계수 적용
+//	float vout = vout_raw * correction;
+//;
+//
+//	// 3. Vout → Vbat (실측 저항값 반영)
+//	float vbat = vout * ((19.7f + 6.73f) / 6.73f);  // ≈ 3.9272
+//
+//	// 4. Vbat → 퍼센트 계산
+//	float percent = ((vbat - 9.6f) / (12.6f - 9.6f)) * 100.0f;
+//
+//	if (percent > 100.0f) percent = 100.0f;
+//	if (percent < 0.0f) percent = 0.0f;
+//
+//	// 5. vout 값 반환 (OLED 출력용)
+//	if (vout_ret != NULL)
+//		*vout_ret = vout;
+//
+//	return percent;
+//}
 /* USER CODE END 0 */
 
 /**
@@ -121,48 +131,61 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC1_Init();
-  MX_I2C1_Init();
+  MX_CAN_Init();
   /* USER CODE BEGIN 2 */
+  HAL_CAN_Start(&hcan);
+  // Configure the filter
+  sFilterConfig.FilterActivation = CAN_FILTER_ENABLE;
+  sFilterConfig.FilterFIFOAssignment = CAN_FILTER_FIFO1;
+  sFilterConfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  sFilterConfig.FilterIdHigh = 0x6A5<<5;
+  sFilterConfig.FilterIdLow = 0;
+  sFilterConfig.FilterMaskIdHigh = 0x7FF<<5; // SET 0 to unfilter
+  sFilterConfig.FilterMaskIdLow = 0;
+  sFilterConfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  HAL_CAN_ConfigFilter(&hcan, &sFilterConfig);
+  // Activate the notification
+  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO1_MSG_PENDING);
+
 
   // OLED 초기화
-  SSD1306_Init();
-  SSD1306_Clear();
-  SSD1306_UpdateScreen();  // 초기 클리어 반영
+//  SSD1306_Init();
+//  SSD1306_Clear();
+//  SSD1306_UpdateScreen();  // 초기 클리어 반영
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 
-  uint32_t last_update_time = HAL_GetTick();
+//  uint32_t last_update_time = HAL_GetTick();
 
   while (1)
   {
-	 uint32_t now = HAL_GetTick();
-
-	 if (now - last_update_time >= 1000)  // 1초 경과 시
-	 {
-		 last_update_time = now;
-
-		 float vout = 0.0f;
-		 float percent = Read_Battery_Percentage(&vout);
-
-		 uint8_t display_percent = (uint8_t)(percent + 0.5f);  // 소수점 반올림
-
-		 char buffer1[20], buffer2[20]; // OLED에 출력할 텍스트 버퍼
-		 snprintf(buffer1, sizeof(buffer1), "BAT: %3d%%", display_percent);
-		 snprintf(buffer2, sizeof(buffer2), "VOL: %.2fV", vout);
-
-		 SSD1306_Clear();
-		 SSD1306_GotoXY(0, 0);
-		 SSD1306_Puts(buffer1, &Font_11x18, 1);
-
-		 SSD1306_GotoXY(0, 20);
-		 SSD1306_Puts(buffer2, &Font_11x18, 1);
-
-		 SSD1306_UpdateScreen();
-	 }
+//	 uint32_t now = HAL_GetTick();
+//
+//	 if (now - last_update_time >= 1000)  // 1초 경과 시
+//	 {
+//		 last_update_time = now;
+//
+//		 float vout = 0.0f;
+//		 float percent = Read_Battery_Percentage(&vout);
+//
+//		 uint8_t display_percent = (uint8_t)(percent + 0.5f);  // 소수점 반올림
+//
+//		 char buffer1[20], buffer2[20]; // OLED에 출력할 텍스트 버퍼
+//		 snprintf(buffer1, sizeof(buffer1), "BAT: %3d%%", display_percent);
+//		 snprintf(buffer2, sizeof(buffer2), "VOL: %.2fV", vout);
+//
+//		 SSD1306_Clear();
+//		 SSD1306_GotoXY(0, 0);
+//		 SSD1306_Puts(buffer1, &Font_11x18, 1);
+//
+//		 SSD1306_GotoXY(0, 20);
+//		 SSD1306_Puts(buffer2, &Font_11x18, 1);
+//
+//		 SSD1306_UpdateScreen();
+//	 }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -178,15 +201,17 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
   */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -196,18 +221,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV2;
-  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
